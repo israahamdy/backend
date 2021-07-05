8c854bde1df044c9dd0195991796fde20db4ce73
israa <esraaabdelsamea99@gmail.com>
2021-07-05 20:08:53 +0200

    modify PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const hostname = 'localhost';
-const port = process.env.Port || 3000;
+const port = process.env.Port || 5000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
